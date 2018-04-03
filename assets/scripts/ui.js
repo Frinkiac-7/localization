15f949f5c35a785761c238b2161a92f2f16ad5a1
frinkiac-7.net <rob@frinkiac-7.net>
2018-04-03 09:17:13 -0400

    Update styling for demo   * Modify colors to highlight page differences

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -2,8 +2,11 @@
 
 const useEnglish = function () {
   $('#favicon-locale').attr('href', './favicon-en.ico')
+  $('#main-view').attr('class', 'container body-en')
   $('#title-locale').text('English')
   $('#pagehead-locale').text('Simple Localization Demo')
+  $('#pagehead-locale').attr('class', 'border-en')
+  $('#main-locale').attr('class', 'body-en')
   $('#body-locale').text('This is a rudimentary example of how localization could be implemented in a rudimentary way.  Obviously, this would be inefficient for anything other than the most basic of applications.  However, it demonstrates the possibilities of using localization to extend the reach of your application to markets outside your native country.')
 }
 
@@ -11,6 +14,8 @@ const useSpanish = function () {
   $('#favicon-locale').attr('href', './favicon-es.ico')
   $('#title-locale').text('Español')
   $('#pagehead-locale').text('Ejemplo simple de localización')
+  $('#pagehead-locale').attr('class', 'border-es')
+  $('#main-locale').attr('class', 'body-es')
   $('#body-locale').text('Esto es un ejemplo simple de cómo se puede usar la localización en su aplicación. Obviamente, este metodo sería suficiente solo para las aplicaciones mas basica. Sin embargo, demuestra las posibilidades para extender el alcance de su aplicación a mercados en países extranjero.')
 }
 
@@ -18,6 +23,8 @@ const useJapanese = function () {
   $('#favicon-locale').attr('href', './favicon-jp.ico')
   $('#title-locale').text('日本の')
   $('#pagehead-locale').text('シンプルなローカリゼーションのデモ')
+  $('#pagehead-locale').attr('class', 'border-jp')
+  $('#main-locale').attr('class', 'body-jp')
   $('#body-locale').text('これは、ローカライゼーションが基本的な方法でどのように実装されるかの基本的な例です。 明らかに、これは最も基本的なアプリケーション以外のものにとっては非効率的である。 ただし、ローカリゼーションを使用してアプリケーションの範囲を本国以外の市場に拡大する可能性があることを示しています。')
 }
 
@@ -25,6 +32,7 @@ const useChinese = function () {
   $('#favicon-locale').attr('href', './favicon-cn.ico')
   $('#title-locale').text('')
   $('#pagehead-locale').text('')
+  $('#body-locale').attr('class', 'border-cn')
   $('#body-locale').text('')
 }
 
